e411964596c6c71cea28ad0cb5554003a68e450e
Maycon <maycon.202.mb@gmail.com>
2024-11-22 11:34:28 -0300

    update de projeto: -faltam configurações de banco de dados e de autenticação.

--- src/main/java/com/example/alimentACAO/AlimentAcaoApplication.java
+++ src/main/java/com/example/alimentACAO/AlimentAcaoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-//Iniciar app Springboot
 @SpringBootApplication
 @EntityScan("com.example.alimentACAO.model")
 public class AlimentAcaoApplication {
